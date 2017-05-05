525ef9f89e384b92069086a91d89110790b9c24f
alexiers <alexiers@gmail.com>
2017-05-06 03:38:49 +0800

    Update AndroidAppVersionCheck.js

--- AndroidAppVersionCheck.js
+++ AndroidAppVersionCheck.js
@@ -1,7 +1,7 @@
 //The purpose of this script is to compare the version between the one users have and the latest one available at the Play Store. If they have an old version, they will be prompt to download the latest version from the app.
 //Inside the app, there will be a variable called "CurrentVersion" which states that app's current version
 //Change this value everytime you update the app on the Play Store
-var LatestVersion = "2.0.0";
+var LatestVersion = "2.1.0";
 
 //This function will run inside the onload function in the app;
         function CheckVersion() {
